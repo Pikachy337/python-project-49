9b4233f7f8192f1ef28c7b2328f0c9ecd1226202
Pikachy <pikachyshkaa@gmail.com>
2024-07-31 23:19:07 +0500

    add new game "Brain Calculator"

---
+++ brain_games/scripts/games/brain_calc.py
@@ -0,0 +1,63 @@
+from random import randint
+
+
+def main():
+
+    print('What is the result of the expression?')  # begin game
+    counter_for_mistakes = 0
+    counter_for_correct_answer = 0
+
+    while counter_for_mistakes != 3 and counter_for_correct_answer != 3:
+        choice_operations = randint(1, 3)
+
+        if choice_operations == 1:  # for +
+            number_1 = randint(0, 100)
+            number_2 = randint(0, 100)
+            print(f'Question: {number_1} + {number_2}')
+            print('Your answer: ', end='')
+            user_answer = int(input())
+            correct_answer = number_1 + number_2
+            if user_answer == correct_answer:
+                counter_for_correct_answer += 1
+                print('Correct!')
+            else:
+                print(f"'{user_answer}' is wrong answer ;(. "
+                      f"Correct answer was '{correct_answer}'")
+                print("Let's try again, NAME!")
+                counter_for_mistakes += 1
+
+        elif choice_operations == 2:  # for -
+            number_1 = randint(0, 100)
+            number_2 = randint(0, 100)
+            print(f'Question: {max(number_1, number_2)} - '
+                  f'{min(number_1, number_2)}')
+            print('Your answer: ', end='')
+            user_answer = int(input())
+            correct_answer = max(number_1, number_2) - min(number_1, number_2)
+            if user_answer == correct_answer:
+                counter_for_correct_answer += 1
+                print('Correct!')
+            else:
+                print(f"'{user_answer}' is wrong answer ;(. "
+                      f"Correct answer was '{correct_answer}'")
+                print("Let's try again, NAME!")
+                counter_for_mistakes += 1
+
+        elif choice_operations == 3:  # for *
+            number_1 = randint(0, 25)
+            number_2 = randint(0, 10)
+            print(f'Question: {number_1} * {number_2}')
+            print('Your answer: ', end='')
+            user_answer = int(input())
+            correct_answer = number_1 * number_2
+            if user_answer == correct_answer:
+                counter_for_correct_answer += 1
+                print('Correct!')
+            else:
+                print(f"'{user_answer}' is wrong answer ;(. "
+                      f"Correct answer was '{correct_answer}'")
+                print("Let's try again, NAME!")
+                counter_for_mistakes += 1
+
+        if counter_for_correct_answer == 3:
+            print('Congratulations, NAME')
---
+++ brain_games/scripts/games/brain_even.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+from random import randint
+
+
+def main():
+    print('Answer "yes" if the number is even, otherwise answer "no".')
+    counter_for_mistakes = 0
+    counter_for_correct_answer = 0
+    while counter_for_mistakes != 3 and counter_for_correct_answer != 3:
+        number = randint(0, 100)  # form a number
+        print(f'Question: {number}')  # question
+        print('Your answer: ', end='')  # user input number
+        user_answer = input()
+        correct_answer = 'yes' if number % 2 == 0 else 'no'
+        if user_answer != correct_answer:  # truth check
+            print(f'{user_answer} is wrong answer ;(. '
+                  f'Correct answer was {correct_answer}')
+            counter_for_mistakes += 1
+            print(f"Let's try again, {name}!")
+        else:
+            print('Correct!')
+            counter_for_correct_answer += 1
