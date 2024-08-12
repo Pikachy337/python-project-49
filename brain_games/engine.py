a0bdbd1a24b111ce3a2651c46330c59bcab59875
Pikachy <pikachyshkaa@gmail.com>
2024-08-12 15:19:26 +0500

    rename file

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -13,7 +13,7 @@ lst_game = ['1. Check for evenness.', '2. Calculator.',
 def main():
     # ceremony
     print('Hello, this is my project for Hexlet №1. ',
-          'Created Dmitry Yarinsky. Edits made : 0',
+          'Created Dmitry Yarinsky. Edits made : 1',
           'Select the game you are interested in:', sep='\n')
     # choice the game
     counter = 0
