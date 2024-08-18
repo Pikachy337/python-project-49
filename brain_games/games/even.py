32b43dc7c7dd4222047916fdb3e6ffdb416ebdeb
Pikachy <pikachyshkaa@gmail.com>
2024-08-18 15:37:45 +0500

    fix lint

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 from random import randint
 
-DESCRIPTION = 'Answer "yes" if number even otherwise answer "no".'
+DESCRIPTION = 'Answer "yes" if the number is even, otherwise answer "no".'
 
 
 def run_game():
